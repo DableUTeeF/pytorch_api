354a3eb155754ef1f6812543d17980517c9f59b7
Natthaphon Hongcharoen <palm22180@gmail.com>
2019-01-30 13:53:18 +0700

    Added predict_generator. Added array fit. Added list metric compiling 	modified:   natthaphon/pytorch_api.py

--- natthaphon/pytorch_api.py
+++ natthaphon/pytorch_api.py
@@ -39,15 +39,31 @@ class Model:
             self.loss = loss
         else:
             self.loss = nn.BCELoss()
-        if metric == 'acc' or metric == 'accuracy':
-            if isinstance(loss, nn.BCELoss):
-                self.metric = [self.bce_accuracy()]
-            elif isinstance(loss, nn.CrossEntropyLoss):  # loss == nn.CrossEntropyLoss():
-                self.metric = [self.categorical_accuracy()]
+        self.metric = []
+        if type(metric) != list:
+            if metric == 'acc' or metric == 'accuracy':
+                if isinstance(loss, nn.BCELoss):
+                    self.metric = [self.bce_accuracy()]
+                elif isinstance(loss, nn.CrossEntropyLoss):  # loss == nn.CrossEntropyLoss():
+                    self.metric = [self.categorical_accuracy()]
+                else:
+                    raise ValueError('String metric should use with nn.BCELoss or nn.CrossEntropyLoss, found {}'.format(self.loss))
             else:
-                raise ValueError('String metric should use with nn.BCELoss or nn.CrossEntropyLoss, found {}'.format(self.loss))
+                self.metric = [metric]
         else:
-            self.metric = [metric]
+            for m in metric:
+                if metric == 'acc' or metric == 'accuracy':
+                    if isinstance(loss, nn.BCELoss):
+                        self.metric.append(self.bce_accuracy())
+                    elif isinstance(loss, nn.CrossEntropyLoss):  # loss == nn.CrossEntropyLoss():
+                        self.metric.append(self.categorical_accuracy())
+                    else:
+                        raise ValueError(
+                            'String metric should use with nn.BCELoss or nn.CrossEntropyLoss, found {}'.format(
+                                self.loss))
+                else:
+                    self.metric.append(m)
+
         self.metric.append(self.loss)
         self.to(device)
 
@@ -131,6 +147,79 @@ class Model:
             for h in history_log:
                 history_log[h] = history_log[h] / len(generator)
         return history_log
+
+    def predict_generator(self, generator):
+        self.lastext = ''
+        self.start_epoch_time = time.time()
+        self.model.eval()
+        prd = []
+        with torch.no_grad():
+            for idx, inputs in enumerate(generator):
+                if type(inputs) == tuple:
+                    inputs = inputs[0]
+                inputs = inputs.to(self.device)
+                prd.append(self.model(inputs))
+        return prd
+
+    def fit(self, x, y, batch_size, epoch, validation_data=None, lrstep=None):
+        if len(x) != len(y):
+            raise ValueError('x and y should have same length')
+        if self.loss is None:
+            self.compile('sgd', None)
+        if lrstep:
+            schedule = torch.optim.lr_scheduler.MultiStepLR(self.optimizer,
+                                                            lrstep)
+        step_per_epoch = round(len(x)/batch_size)
+        log = {}
+        try:
+            for e in range(epoch):
+                print('Epoch:', e+1)
+                self.lastext = ''
+                self.start_epoch_time = time.time()
+                self.last_print_time = self.start_epoch_time
+                total = 0
+                self.model.train()
+                progbar = Progbar(len(x))
+                history_log = {}
+                for idx in range(0, len(x), batch_size):
+                    inputs, targets = x[idx:idx+batch_size], y[idx:idx+batch_size]
+                    inputs = inputs.to(self.device)
+                    targets = targets.to(self.device)
+                    output = self.model(inputs)
+                    printlog = []
+                    for metric in self.metric:
+                        m_out = metric(output, targets)
+                        if metric.__str__()[:-2] not in history_log:
+                            history_log[metric.__str__()[:-2]] = m_out.cpu().detach().numpy()
+                            printlog.append([metric.__str__()[:-2], m_out.cpu().detach().numpy()])
+                        else:
+                            history_log[metric.__str__()[:-2]] += m_out.cpu().detach().numpy()
+                            printlog.append([metric.__str__()[:-2], history_log[metric.__str__()[:-2]]/(idx+1)])
+
+                    self.optimizer.zero_grad()
+                    m_out.backward()
+                    self.optimizer.step()
+                    total += inputs.size(0)
+
+                    progbar.update(idx-1, printlog)
+
+                for h in history_log:
+                    history_log[h] = history_log[h] / step_per_epoch
+                metrics = []
+                if validation_data:
+                    val_metrics = self.evaluate_generator(validation_data)
+                    for metric in val_metrics:
+                        metrics.append(['val_'+metric, val_metrics[metric]])
+                progbar.update(step_per_epoch, metrics, force=True)
+                if lrstep:
+                    schedule.step()
+                for key in history_log:
+                    if key not in log:
+                        log[key] = []
+                    log[key].append(history_log[key])
+
+        finally:
+            return log
 
     class bce_accuracy:
         def __call__(self, inputs, targets):
