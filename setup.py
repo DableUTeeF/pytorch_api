18aca4e2a4abe80185a3fff418e455cf14a86a00
DableUTeeF <palm22180@gmail.com>
2019-03-18 22:35:32 +0700

    Added anm aart 	modified:   .gitignore 	modified:   natthaphon/__init__.py 	new file:   natthaphon/aart/anm.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 
 setuptools.setup(
     name="natthaphon",
-    version="0.1.6",
+    version="0.2.1",
     author="Natthaphon Hongcharoen",
     author_email="palm22180@gmail.com",
-    description="A small package for my usage",
+    description="Added anm aart",
     long_description="No need for any description",
     long_description_content_type="text/markdown",
     url="https://github.com/DableUTeeF/pytorch_api",
